f6815b1beb3ba489b953a21548c72dabbeaf5bad
AltF0x4 <dumitru.silviu@proton.me>
2026-06-29 12:40:24 +0300

    Update med-sign-pass.js

--- med-sign-pass.js
+++ med-sign-pass.js
@@ -193,8 +193,7 @@ async function boot() {
      // If phone number is required, you need to pass it in the sign-up request
      // If phone number is optional, you can skipp it
      // const payload = { email, phone_number: phone, password: pw.value };
-     const amd_phone = {"phone": phone}
-     const payload = { email, password: pw.value, app_metadata: amd_phone};
+     const payload = { email, password: pw.value, 'ulp-phone': phone};
     if (acul) {
       acul.signup(payload);            // → Auth0 creates the user / completes signup
     } else {
